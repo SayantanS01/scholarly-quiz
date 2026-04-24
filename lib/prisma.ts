2c2413b4e3794bcda9cf15cbd485f8ecde8b3f92
SAYANTAN SARKAR <hi2sayantan@gmail.com>
2026-04-24 10:20:21 +0530

    Fix Prisma 7 compatibility: move URL to config and constructor

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,7 +5,9 @@ const globalForPrisma = global as unknown as { prisma: any };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient().$extends(withAccelerate());
+  new PrismaClient({
+    accelerateUrl: process.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
